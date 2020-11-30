443862032030821c18336797ad956f2fe025b638
Roman Chugai <r.chugai@urancompany.com>
2020-11-30 18:45:23 +0200

    testing configuration

--- Example/CMP/ViewController.swift
+++ Example/CMP/ViewController.swift
@@ -14,30 +14,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-
-        
-       
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     @IBAction func testAction(_ sender: Any) {
-        let filePath = Bundle.main.path(forResource: "cmp", ofType: "html")
-                do {
-                    let jsContent = try String.init(contentsOfFile: filePath!, encoding: String.Encoding.utf8)
-                    let context = OpenCmpContext(domen: jsContent, key: "key")
-                    OpenCmp.config.activateWithContext(context: context)
-                }
-                catch let error as NSError{
-                    print(error.debugDescription)
-                }
+        testWithHtmlFile()
     }
     
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func testWithHtmlFile() {
+        let filePath = Bundle.main.path(forResource: "cmp", ofType: "html")
+        do {
+            let jsContent = try String.init(contentsOfFile: filePath!, encoding: String.Encoding.utf8)
+            let context = OpenCmpContext(domen: jsContent, key: "key")
+            OpenCmp.config.activateWithContext(context: context)
+        }
+        catch let error as NSError{
+            print(error.debugDescription)
+        }
     }
+
 
 }
 
