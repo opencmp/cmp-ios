a52f8aed689b2c6b30b0fb184c5e4bc6b7b16238
Kovtun Dmitriy <mp.mobidev@gmail.com>
2021-01-12 17:03:58 +0200

    add description of initializing

--- Example/CMP/ViewController.swift
+++ Example/CMP/ViewController.swift
@@ -3,8 +3,10 @@ import CMP
 import UIKit
 
 class ViewController: UIViewController {
+//in your class or AppDelegate
     override func viewDidLoad() {
         super.viewDidLoad()
+        //creating config with domain, storageName, errorHandler and data changeListener
         let config = OpenCmpConfig(
             "traffective.com",
             setStorageName: "open_cmp.storage",
@@ -13,15 +15,17 @@ class ViewController: UIViewController {
             }, setChangesListener: { change in
                 print("CMP change", change.value)
             })
+        //initialize framework
         OpenCmp.initialize(self, config)
-        OpenCmp.showUI()
     }
 
     @IBAction func showPopup(_ sender: Any) {
+        //show UI
         OpenCmp.showUI()
 
     }
     @IBAction func cleanData(_ sender: Any) {
+        //clear saved data
         OpenCmp.cleanUserDefaults()
     }
     
